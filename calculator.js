8878a56835ff2544ea835f7cc0dad143308ce42b
Aditya Dev <prakash.adityaprakash.aditya23@gmail.com>
2024-10-19 15:49:46 +0530

    two number (comma separated)

--- calculator.js
+++ calculator.js
@@ -3,7 +3,6 @@ function add(numbers) {
         return 0;
     }
 
-    if (!numbers.includes(',')) {
-        return parseInt(numbers);
-    }
+    const numArray = numbers.split(',');
+    return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
